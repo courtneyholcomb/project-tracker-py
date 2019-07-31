a1505d7213e9685cfb4378ce3eab3f16aef03728
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-30 17:26:04 -0700

    Completed all functions and added all commands to handle_input()

--- hackbright.py
+++ hackbright.py
@@ -42,22 +42,68 @@ def make_new_student(first_name, last_name, github):
     Given a first name, last name, and GitHub account, add student to the
     database and print a confirmation message.
     """
-    pass
+    QUERY = """
+        INSERT INTO students (first_name, last_name, github)
+            VALUES (:first_name, :last_name, :github)
+        """
+
+    db.session.execute(QUERY, {'first_name': first_name,
+                                'last_name': last_name,
+                                'github': github})
+
+    db.session.commit()
+
+    print(f"Successfully added student: {first_name} {last_name}")    
 
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
-    pass
+    QUERY = """
+        SELECT title, description, max_grade
+        FROM projects
+        WHERE title = :title
+        """
+
+    db_cursor = db.session.execute(QUERY, {'title': title})
+
+    row = db_cursor.fetchone()
+
+    print(f"Title: {row[0]} \nDescription: {row[1]} \nMax Grade: {row[2]}")
+
 
 
 def get_grade_by_github_title(github, title):
     """Print grade student received for a project."""
-    pass
+    QUERY = """
+        SELECT student_github, project_title, grade
+        FROM grades
+        WHERE student_github = :student_github 
+            AND project_title = :project_title
+        """
+
+    db_cursor = db.session.execute(QUERY, {'student_github': github, 
+                                            'project_title': title})
+
+    row = db_cursor.fetchone()
+
+    print(f"Student: {row[0]} \nProject: {row[1]} \nGrade: {row[2]}")
 
 
 def assign_grade(github, title, grade):
     """Assign a student a grade on an assignment and print a confirmation."""
-    pass
+    QUERY = """
+        INSERT INTO grades (student_github, project_title, grade)
+            VALUES (:student_github, :project_title, :grade)
+        """
+
+    db.session.execute(QUERY, {'student_github': github,
+                                'project_title': title,
+                                'grade': grade})
+
+    db.session.commit() 
+
+    print(f"Successfully added grade: {grade}")    
+
 
 
 def handle_input():
@@ -74,7 +120,7 @@ def handle_input():
         tokens = input_string.split()
         command = tokens[0]
         args = tokens[1:]
-
+        
         if command == "student":
             github = args[0]
             get_student_by_github(github)
@@ -82,6 +128,21 @@ def handle_input():
         elif command == "new_student":
             first_name, last_name, github = args  # unpack!
             make_new_student(first_name, last_name, github)
+
+        elif command == "project":
+            title = args[0]
+            get_project_by_title(title)
+
+        elif command == "get_grade":
+            github = args[0]
+            project = args[1]
+            get_grade_by_github_title(github, title)
+
+        elif command == "assign_grade":
+            github = args[0]
+            project = " ".join(args[1:-1])
+            grade = args[-1]
+            assign_grade(github, project, grade)
 
         else:
             if command != "quit":
@@ -91,7 +152,7 @@ def handle_input():
 if __name__ == "__main__":
     connect_to_db(app)
 
-    # handle_input()
+    handle_input()
 
     # To be tidy, we close our database connection -- though,
     # since this is where our program ends, we'd quit anyway.
